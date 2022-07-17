99943283b0d27aaa3d337b3a7cfcaa281ac55f5b
Rômulo Fernandes Evangelista <romfe89@gmail.com>
2022-07-17 12:47:45 -0300

    primeiro commit

---
+++ todo_backend/src/main/kotlin/com/romulo/todo_backend/TodoBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.romulo.todo_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoBackendApplication>(*args)
+}
---
+++ todo_backend/src/test/kotlin/com/romulo/todo_backend/TodoBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.romulo.todo_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
